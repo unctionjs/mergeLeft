f31ea3a5f9fe87f3e8cb6bde120b71184d1f4a13
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-07 14:39:14 -0700

    Copying over

--- test.js
+++ test.js
@@ -1,12 +1,46 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import tempLate from "./"
+import mergeLeft from "./"
 
 test(({same, end}) => {
+  const left = {
+    alpha: "1",
+    beta: "1",
+  }
+  const right = {
+    beta: "2",
+    zeta: "3",
+  }
+
   same(
-    tempLate(true),
-    false
+    mergeLeft(left)(right),
+    {
+      alpha: "1",
+      beta: "1",
+      zeta: "3",
+    }
+  )
+
+  end()
+})
+
+test(({same, end}) => {
+  const left = [
+    "a",
+    "b",
+  ]
+  const right = [
+    "c",
+  ]
+
+  same(
+    mergeLeft(left)(right),
+    [
+      "c",
+      "a",
+      "b",
+    ]
   )
 
   end()
