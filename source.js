fa5dd19b7ec942f7032d7a54a9c63c063b6185e2
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-14 02:41:42 -0700

    Adding more types and fixing flow fuckups

--- source.js
+++ source.js
@@ -1,40 +1,30 @@
-import {all} from "ramda"
-import couple from "@unction/couple"
-import isObject from "@unction/isobject"
-import isArray from "@unction/isarray"
-import isPresent from "@unction/ispresent"
+/* eslint-disable no-extra-parens */
+import type from "@unction/type"
+import key from "@unction/key"
+import xstream from "xstream"
 
-const isArrayPair = all(isArray)
-const isObjectPair = all(isObject)
+const mapping = {
+  Array: (left: ArrayType): Function => (right: ArrayType): ArrayType => [
+    ...right,
+    ...left,
+  ],
+  Object: (left: ObjectType): Function => (right: ObjectType): ObjectType => ({
+    ...right,
+    ...left,
+  }),
+  // "Map": (left): Function => (right) => new Error("I have no idea how to merge a Map"),
+  // "WeakMap": (left): Function => (right) => new Error("I have no idea how to merge a WeakMap"),
+  // "Set": (left) => (right): Function => new Error("I have no idea how to merge a Set"),
+  // "WeakSet": (left): Function => (right) => new Error("I have no idea how to merge a WeakSet"),
+  String: (left: string): Function => (right: string): string => `${left}${right}`,
+  // "Buffer": (left): Function => (right) => new Error("I have no idea how to merge a Buffer"),
+  Stream: (left: StreamType): Function => (right: StreamType): StreamType => xstream.merge(right, left),
+}
 
 export default function mergeLeft (left: IterableType): Function {
-  const pairWithLeft = couple(left)
+  const leftType = type(left)
 
   return function mergeLeftLeft (right: IterableType): IterableType {
-    const pair = pairWithLeft(right)
-
-    if (!isPresent(left) || !isPresent(left)) {
-      throw new Error("left or right was not an iterable")
-    }
-
-    if (left.constructor.name !== right.constructor.name) {
-      throw new Error("left and right weren't the same type")
-    }
-
-    if (isArrayPair(pair)) {
-      return [
-        ...right,
-        ...left,
-      ]
-    }
-
-    if (isObjectPair(pair)) {
-      return {
-        ...right,
-        ...left,
-      }
-    }
-
-    throw new Error("Couldn't figure out how to merge left and right")
+    return key(leftType)(mapping)(left)(right)
   }
 }
