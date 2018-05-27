f875735f280a48bd00eeab74912d185d24e8f967
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:29 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,53 +1,49 @@
 /* eslint-disable max-statements */
-import type from "@unction/type"
-import {merge} from "most"
-
-export default function mergeLeft (left: FunctorType): UnaryFunctionType {
-  return function mergeLeftLeft (right: FunctorType): FunctorType {
+import type from "@unction/type";
+import { merge } from "most";
+export default function mergeLeft(left) {
+  return function mergeLeftLeft(right) {
     if (type(left) !== type(right)) {
-      throw new Error(`mergeLeft received a ${type(left)} and ${type(right)} which aren't the same`)
+      throw new Error(`mergeLeft received a ${type(left)} and ${type(right)} which aren't the same`);
     }
 
     switch (type(left)) {
-      case "Array": {
-        return [
-          ...right,
-          ...left,
-        ]
-      }
-
-      case "Object": {
-        return {
-          ...right,
-          ...left,
+      case "Array":
+        {
+          return [...right, ...left];
         }
-      }
 
-      case "Map": {
-        return new Map([
-          ...right,
-          ...left,
-        ])
-      }
+      case "Object":
+        {
+          return { ...right,
+            ...left
+          };
+        }
 
-      case "Set": {
-        return new Set([
-          ...right,
-          ...left,
-        ])
-      }
+      case "Map":
+        {
+          return new Map([...right, ...left]);
+        }
 
-      case "String": {
-        return `${right}${left}`
-      }
+      case "Set":
+        {
+          return new Set([...right, ...left]);
+        }
 
-      case "Stream": {
-        return merge(right, left)
-      }
+      case "String":
+        {
+          return `${right}${left}`;
+        }
 
-      default: {
-        throw new Error(`mergeLeft doesn't know how to deal with ${type(left)}`)
-      }
+      case "Stream":
+        {
+          return merge(right, left);
+        }
+
+      default:
+        {
+          throw new Error(`mergeLeft doesn't know how to deal with ${type(left)}`);
+        }
     }
-  }
+  };
 }
